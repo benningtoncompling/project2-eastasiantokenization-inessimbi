9c2263cb265b82a44d885d551bc2e44b43cb0f59
inessimbi <inessimbi@bennington.edu>
2019-03-18 11:05:08 -0400

    updated japanese_tokenizer.py

--- Japanese Tokenizer/japanese_tokenizer.py
+++ Japanese Tokenizer/japanese_tokenizer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-# clean_and_count_tokens.py - Counting words i a file
+# japanese_tokenizer.py - adding space between Japanese words
 # Author: <Ines Simbi>(inessimbi@bennington.edu)
 # Date: <03.13.2019>
 #
@@ -11,29 +11,33 @@ input_file = sys.argv[1]
 output_file = sys.argv[2]
 
 
-def tokenize (line, japanese_text_dict):
-	the_list = [] # list to save words when word boundary is found
+def tokenize(line, japanese_text_dict):
+	the_list = [] # list to save items when word boundary is found
 	#while loop to go through the line and check if word found in line exists in japanese dictionary
-	the_string= ""
 	i = 0
-	while (i< len(line)):
-		if line[i] in japanese_text_dict:
-			the_list.append(line[i])
-			print(the_list)
-			#return the_list
+	while (i < len(line)): # gets item from line
+		if line[i] in japanese_text_dict: # statement to check if item in line is in japanese_text_dict
+			the_list.append(line[i]) # adds item in list by appending it to the_list
+			#print(the_list)
+
 		else:
-			current_char = line[i]
+			start_index = i # holds the value of the current index that wasn't found in the dictionary
+			current_char = line[i] # holds value of the current character not found in dictionary
 			found = False
-			while (i < (len (line)-1) and (not found)):
+			while (i < (len (line)-1) and (not found)): #loop to add next character to the previous character if not found in the dictionary
 				current_char = current_char + line[i+1]
 				if current_char in japanese_text_dict:
-					the_list.append(current_char)
+					the_list.append(current_char) #appends group of characters when found to be a word and appends to the list
 					found = True
 				else:
 					i = i+1
-	the_string = the_string.join(the_list)
-	print(the_list)
-	open_out.write(the_string)
+			if not found: # if the index is not found in the dictionary
+				i= start_index
+				the_list.append(line[i]) # the character at index i is going to be treated as its own word
+				i = i + 1 # increments index i to the next index and start going in the while loop
+	the_string = ' '.join(the_list) # joins items in a list by space
+	return the_string
+
 
 
 in_file= "in.txt"
@@ -52,6 +56,7 @@ with open(in_file, 'r', encoding='utf-8') as open_in:
 		for line in open_in.readlines():
 			#print(line)
 			the_line = tokenize(line, japanese_text_dict)
+			open_out.write(the_line) # prints results in out_file
 
 
 
